7c402c0be680daf0d755487ad306b0f8c9e12b8c
Felipe Santos <felipesantos@felipes-imac.home>
2019-09-04 00:08:30 -0300

    Remove projeto lixo

--- sca-web/src/app/components/register/register.component.ts
+++ sca-web/src/app/components/register/register.component.ts
@@ -13,4 +13,23 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  registerUser(login: string, password: string, name: string) {
+
+    let registerRequest = {
+      login,
+      senha: password,
+      nome: name
+    };
+
+    this.loginService.login(loginRequest)
+      .subscribe(
+        (sucess) => {
+          this.router.navigate(['home']);
+        },
+        (err) => {
+          this.snackBar.open(err.error.response);
+        }
+      );
+  }
+
 }
