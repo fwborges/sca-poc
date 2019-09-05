79222b99130fa242d325217f90e5b73750ca57b6
Felipe <felipe.wsantos@grupopan.com>
2019-09-05 16:19:39 -0300

    Adiciona validadores e exceptionhandler

---
+++ modulo-gestao-ativo/src/main/java/com/br/puc/modulogestaoativo/common/GestaoAtivoExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.br.puc.modulogestaoativo.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@ControllerAdvice
+public class GestaoAtivoExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> genericException(MethodArgumentNotValidException e) {
+
+        log.error("Error", e.getMessage());
+
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegalArgumentException(IllegalArgumentException e) {
+
+        log.error("Error", e.getMessage());
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
